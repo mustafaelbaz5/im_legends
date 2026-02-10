1089bba7572cde9f889dab65e935e8f6fd1d06a6
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-10 02:29:42 +0200

    Update MaterialApp Stup For add Theming and localization

--- lib/im_legends_app.dart
+++ lib/im_legends_app.dart
@@ -1,29 +1,46 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:go_router/go_router.dart';
-import 'core/utils/app_assets.dart';
+import 'package:im_legends/core/router/app_router.dart';
+import 'package:im_legends/core/router/route_paths.dart';
+import 'package:im_legends/core/themes/cubit/theme_cubit.dart';
+import 'package:im_legends/core/themes/theme_data/theme_data_dark.dart';
+import 'package:im_legends/core/themes/theme_data/theme_data_light.dart';
 
 class IMLegendsApp extends StatelessWidget {
-  const IMLegendsApp({super.key, required this.router});
-
-  final GoRouter router;
-
+  const IMLegendsApp({super.key, required this.appRouter});
+  final AppRouter appRouter;
   @override
-  Widget build(BuildContext context) {
+  Widget build(final BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(393, 852),
       minTextAdapt: true,
-      builder: (context, child) {
-        return MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          title: 'IM Legends App',
-          theme: ThemeData(
-            scaffoldBackgroundColor: const Color(0xFF000000),
-            fontFamily: AppAssets.fontRoboto,
+      splitScreenMode: true,
+      builder: (final BuildContext context, final Widget? child) {
+        return BlocProvider(
+          create: (final BuildContext context) => ThemeCubit(),
+
+          child: BlocBuilder<ThemeCubit, ThemeMode>(
+            builder: (final BuildContext context, final ThemeMode mode) {
+              return MaterialApp(
+                key: ValueKey(context.locale),
+                localizationsDelegates: context.localizationDelegates,
+                supportedLocales: context.supportedLocales,
+                locale: context.locale,
+                debugShowCheckedModeBanner: false,
+                initialRoute: Routes.onBoardingScreen,
+                onGenerateRoute: appRouter.generateRoute,
+                title: 'app_title'.tr(),
+                theme: getLightTheme(context: context),
+                darkTheme: getDarkTheme(context: context),
+                themeMode: mode,
+              );
+            },
           ),
-          routerConfig: router, // Use GoRouter here
         );
       },
     );
   }
 }
+// test123@gmail.com
