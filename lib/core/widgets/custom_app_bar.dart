3dbdd97e0a334da9c7c241c3e3cd5fb171d72744
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-11 18:56:07 +0200

    Add Notification Icon

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -1,107 +1,42 @@
-// custom_app_bar.dart
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:go_router/go_router.dart';
-import 'package:im_legends/core/router/route_paths.dart';
-import 'package:im_legends/features/notification/logic/cubit/notifications_cubit.dart';
-import 'package:im_legends/features/profile/logic/cubit/profile_cubit.dart';
+import 'package:im_legends/core/utils/extensions/context_extensions.dart';
+import 'package:im_legends/core/utils/functions/app_setting_method.dart';
+import 'package:im_legends/core/widgets/notification_icon.dart';
 
 import '../themes/app_texts_style.dart';
-import '../utils/app_assets.dart';
 import '../utils/spacing.dart';
-import 'notification_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
   final String title;
   const CustomAppBar({super.key, required this.title});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 40, left: 20, right: 20, bottom: 10),
-      decoration: const BoxDecoration(
-        color: Color(0xFF191919),
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
-        boxShadow: [
-          BoxShadow(
-            color: Color(0xFF323743),
-            offset: Offset(0, 4),
-            blurRadius: 8,
-          ),
-        ],
+  Widget build(final BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: responsiveWidth(24),
+        vertical: responsiveHeight(16),
       ),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+
         children: [
-          Expanded(
-            child: Center(
-              child: Text(title, style: BebasTextStyles.whiteBold20),
-            ),
+          Text(
+            title,
+            style: AppTextStyles.font20Bold,
+            textAlign: TextAlign.center,
           ),
-          // Notifications from cubit
-          BlocBuilder<NotificationsCubit, NotificationsState>(
-            builder: (context, state) {
-              if (state is NotificationsSuccess) {
-                final unreadCount = state.notifications
-                    .where((notification) => !notification.isRead)
-                    .length;
-                return NotificationIcon(unreadCount: unreadCount);
-              } else {
-                return const NotificationIcon(unreadCount: 0);
-              }
-            },
-          ),
-          horizontalSpacing(8),
+          const Spacer(),
           GestureDetector(
-            onTap: () => context.go(Routes.profileScreen),
+            onTap: () {
+              switchTheme(context);
+            },
             child: CircleAvatar(
-              radius: 20,
-              backgroundColor: Colors.white,
-              child: ClipOval(
-                child: SizedBox(
-                  width: 36,
-                  height: 36,
-                  child: BlocBuilder<ProfileCubit, ProfileState>(
-                    builder: (context, state) {
-                      if (state is ProfileSuccess) {
-                        final profile = state.player.user;
-                        if (profile.profileImageUrl == null ||
-                            profile.profileImageUrl!.isEmpty) {
-                          return SvgPicture.asset(
-                            AppAssets.appSvgLogo,
-                            fit: BoxFit.cover,
-                            width: 36,
-                            height: 36,
-                          );
-                        }
-                        return CachedNetworkImage(
-                          imageUrl: profile.profileImageUrl!,
-                          fit: BoxFit.cover,
-                          width: 36,
-                          height: 36,
-                          placeholder: (context, url) =>
-                              const CircularProgressIndicator(strokeWidth: 2),
-                          errorWidget: (context, url, error) =>
-                              SvgPicture.asset(
-                                AppAssets.appSvgLogo,
-                                fit: BoxFit.cover,
-                                width: 36,
-                                height: 36,
-                              ),
-                        );
-                      } else {
-                        return SvgPicture.asset(
-                          AppAssets.appSvgLogo,
-                          fit: BoxFit.cover,
-                          width: 36,
-                          height: 36,
-                        );
-                      }
-                    },
-                  ),
-                ),
+              radius: responsiveRadius(20),
+              backgroundColor: context.customColors.divider.withValues(
+                alpha: 0.5,
               ),
+              child: const NotificationIcon(unreadCount: 3),
             ),
           ),
         ],
--- lib/core/widgets/notification_icon.dart
+++ lib/core/widgets/notification_icon.dart
@@ -1,47 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-
-import '../router/route_paths.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:im_legends/core/router/routes.dart';
+import 'package:im_legends/core/themes/app_colors.dart';
+import 'package:im_legends/core/themes/app_texts_style.dart';
+import 'package:im_legends/core/utils/app_assets.dart';
+import 'package:im_legends/core/utils/extensions/context_extensions.dart';
 
 class NotificationIcon extends StatelessWidget {
   final int unreadCount;
   const NotificationIcon({super.key, required this.unreadCount});
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        IconButton(
-          icon: const Icon(Icons.notifications_rounded, color: Colors.white),
-          onPressed: () {
-            context.push(Routes.notificationsScreen);
-          },
-        ),
-        if (unreadCount > 0)
-          Positioned(
-            right: 6,
-            top: 6,
-            child: Container(
-              padding: const EdgeInsets.all(4),
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                shape: BoxShape.circle,
-              ),
-              constraints: const BoxConstraints(minWidth: 18, minHeight: 18),
-              child: Center(
-                child: Text(
-                  unreadCount > 9 ? '9+' : unreadCount.toString(),
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 10,
-                    fontWeight: FontWeight.bold,
+  Widget build(final BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        context.pushNamed(Routes.notificationsScreen);
+      },
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          SvgPicture.asset(
+            AppAssets.notificationIconSvg,
+            colorFilter: const ColorFilter.mode(
+              AppColors.primary300,
+              BlendMode.srcIn,
+            ),
+          ),
+          if (unreadCount > 0)
+            Positioned(
+              right: 6,
+              top: 6,
+              child: Container(
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  color: context.customColors.errorContainer,
+                  shape: BoxShape.circle,
+                ),
+                constraints: const BoxConstraints(minWidth: 18, minHeight: 18),
+                child: Center(
+                  child: Text(
+                    unreadCount > 9 ? '9+' : unreadCount.toString(),
+                    style: AppTextStyles.font12SemiBold,
                   ),
                 ),
               ),
             ),
-          ),
-      ],
+        ],
+      ),
     );
   }
 }
