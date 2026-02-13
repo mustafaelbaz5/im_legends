4433beb0bcc9d4d432489663d3e0516209c38b47
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-13 09:19:09 +0200

    Rebuild AddMatchScreen For Better ui and colors & init cubit for ui controll with improved and cleared logic

--- lib/features/add_match/ui/add_match_screen.dart
+++ lib/features/add_match/ui/add_match_screen.dart
@@ -1,109 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:im_legends/core/themes/app_colors.dart';
+import 'package:im_legends/core/utils/extensions/context_extensions.dart';
+import 'package:im_legends/core/widgets/custom_app_bar.dart';
+import 'package:im_legends/features/add_match/logic/cubit/add_match_cubit.dart';
+import 'package:im_legends/features/add_match/ui/widgets/add_match_bloc_consumer.dart';
+import 'package:im_legends/features/add_match/ui/widgets/player_select_field/player_select_field.dart';
+import 'package:im_legends/features/add_match/ui/widgets/score_input_field.dart';
 
-import '../../../core/themes/text_styles/bebas_text_styles.dart';
 import '../../../core/utils/spacing.dart';
-import 'widgets/add_match_app_bar.dart';
-import 'widgets/add_match_bloc_consumer.dart';
-import 'widgets/player_select_field/player_select_field.dart';
-import 'widgets/score_input_field/score_count_field.dart';
 
-class AddMatchScreen extends StatefulWidget {
+class AddMatchScreen extends StatelessWidget {
   const AddMatchScreen({super.key});
 
-  @override
-  State<AddMatchScreen> createState() => _AddMatchScreenState();
-}
-
-class _AddMatchScreenState extends State<AddMatchScreen> {
-  int winnerScore = 0;
-  int loserScore = 0;
-  String? winnerPlayer;
-  String? loserPlayer;
-
-  void _onWinnerScoreChanged(int score) {
-    setState(() {
-      winnerScore = score;
-      if (loserScore > winnerScore) {
-        loserScore = winnerScore;
-      }
-    });
+  Future<void> onRefresh(final BuildContext context) async {
+    context.read<AddMatchCubit>().resetMatchData();
+    context.read<AddMatchCubit>().getPlayersList();
+    await Future.delayed(const Duration(milliseconds: 300));
   }
-
-  void _onLoserScoreChanged(int score) {
-    setState(() {
-      loserScore = score;
-    });
-  }
-
-  void _onWinnerPlayerChanged(String playerId) {
-    setState(() {
-      winnerPlayer = playerId;
-    });
-  }
-
-  void _onLoserPlayerChanged(String playerId) {
-    setState(() {
-      loserPlayer = playerId;
-    });
-  }
-
-  bool get _isAddButtonEnabled =>
-      winnerScore > loserScore &&
-      winnerPlayer != null &&
-      loserPlayer != null &&
-      winnerPlayer != loserPlayer;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+  Widget build(final BuildContext context) {
+    return SafeArea(
+      child: Column(
         children: [
-          const AddMatchAppBar(),
+          const CustomAppBar(title: 'Add Match'),
           Expanded(
-            child: Column(
-              children: [
-                verticalSpacing(20),
-                Text(
-                  'Select Players and Scores',
-                  style: BebasTextStyles.whiteBold24.copyWith(
-                    wordSpacing: 1.5,
-                    color: Colors.grey.shade300,
-                  ),
+            child: RefreshIndicator(
+              onRefresh: () => onRefresh(context),
+              backgroundColor: context.customColors.background,
+              color: context.customColors.textPrimary,
+              child: SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                padding: EdgeInsets.symmetric(horizontal: responsiveWidth(16)),
+                child: BlocBuilder<AddMatchCubit, AddMatchState>(
+                  builder: (final context, final state) {
+                    return Column(
+                      children: [
+                        verticalSpacing(20),
+
+                        // Winner
+                        PlayerSelectField(
+                          hint: 'Select Winner',
+                          accentColor: AppColors.green100,
+                          selectedPlayerId: state.winnerId,
+                          selectedName: state.winnerName,
+                          selectedImageUrl: state.winnerImage,
+                          excludedPlayer: state.loserId, // exclude loser
+                          isWinnerField: true,
+                          onSelected: (final id, final name, final image) {
+                            context.read<AddMatchCubit>().updateWinner(
+                              id,
+                              name,
+                              image,
+                            );
+                          },
+                        ),
+                        verticalSpacing(16),
+                        const ScoreInputField(
+                          accentColor: AppColors.green100,
+                          isWinner: true,
+                        ),
+                        verticalSpacing(80),
+
+                        // Loser
+
+                        /// Loser field
+                        PlayerSelectField(
+                          hint: 'Select Loser',
+                          accentColor: AppColors.red100,
+                          selectedPlayerId: state.loserId,
+                          selectedName: state.loserName,
+                          selectedImageUrl: state.loserImage,
+                          excludedPlayer: state.winnerId, // exclude winner
+                          isWinnerField: false,
+                          onSelected: (final id, final name, final image) {
+                            context.read<AddMatchCubit>().updateLoser(
+                              id,
+                              name,
+                              image,
+                            );
+                          },
+                        ),
+                        verticalSpacing(16),
+                        const ScoreInputField(
+                          accentColor: AppColors.red100,
+                          isWinner: false,
+                        ),
+                        verticalSpacing(100),
+
+                        // Submit button
+                        const AddMatchBlocConsumer(),
+                      ],
+                    );
+                  },
                 ),
-                verticalSpacing(20),
-                PlayerSelectField(
-                  hint: 'Select Winner',
-                  onSelected: _onWinnerPlayerChanged,
-                  excludedPlayer: loserPlayer,
-                ),
-                verticalSpacing(5),
-                ScoreCountField(
-                  accentColor: Colors.green,
-                  initialScore: winnerScore,
-                  onScoreChanged: _onWinnerScoreChanged,
-                ),
-                verticalSpacing(50),
-                PlayerSelectField(
-                  hint: 'Select Loser',
-                  onSelected: _onLoserPlayerChanged,
-                  excludedPlayer: winnerPlayer,
-                ),
-                verticalSpacing(5),
-                ScoreCountField(
-                  accentColor: Colors.red,
-                  initialScore: loserScore,
-                  onScoreChanged: _onLoserScoreChanged,
-                  maxScore: winnerScore,
-                ),
-                verticalSpacing(50),
-                AddMatchBlocConsumer(
-                  isAddButtonEnabled: _isAddButtonEnabled,
-                  winnerPlayer: winnerPlayer,
-                  loserPlayer: loserPlayer,
-                  winnerScore: winnerScore,
-                  loserScore: loserScore,
-                ),
-              ],
+              ),
             ),
           ),
         ],
