f8fe87d6371724af054318bb808729cfc425f142
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-13 09:18:04 +0200

    Refactor AddMatchConsumer for better ui managing and clean and clear logic

--- lib/features/add_match/ui/widgets/add_match_bloc_consumer.dart
+++ lib/features/add_match/ui/widgets/add_match_bloc_consumer.dart
@@ -1,75 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'success_dialog.dart';
-import '../../../notification/logic/cubit/notifications_cubit.dart';
-import '../../../champion/logic/cubit/champion_cubit.dart';
-import '../../../profile/logic/cubit/profile_cubit.dart';
-import '../../../history/logic/cubit/match_history_cubit.dart';
-import '../../../home/logic/cubit/leader_board_cubit.dart';
-import '../../data/models/match_model.dart';
-import '../../logic/cubit/add_match_cubit.dart';
-import 'add_match_button.dart';
+import 'package:im_legends/core/ui/dialogs/app_dialogs.dart';
+import 'package:im_legends/core/ui/loaders/overlay_loader.dart';
+import 'package:im_legends/core/utils/spacing.dart';
+import 'package:im_legends/core/widgets/custom_text_button.dart';
+import 'package:im_legends/features/add_match/data/models/match_model.dart';
+import 'package:im_legends/features/add_match/logic/cubit/add_match_cubit.dart';
 
 class AddMatchBlocConsumer extends StatelessWidget {
-  const AddMatchBlocConsumer({
-    super.key,
-    required bool isAddButtonEnabled,
-    required this.winnerPlayer,
-    required this.loserPlayer,
-    required this.winnerScore,
-    required this.loserScore,
-  }) : _isAddButtonEnabled = isAddButtonEnabled;
-
-  final bool _isAddButtonEnabled;
-  final String? winnerPlayer;
-  final String? loserPlayer;
-  final int winnerScore;
-  final int loserScore;
+  const AddMatchBlocConsumer({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(final BuildContext context) {
     return BlocConsumer<AddMatchCubit, AddMatchState>(
-      listener: (context, state) {
-        if (state is AddMatchInsertSuccess) {
-          context.read<NotificationsCubit>().handleMatchResult(
-            winnerId: winnerPlayer!,
-            loserId: loserPlayer!,
-          );
-          context.read<LeaderBoardCubit>().loadLeaderboard();
-          context.read<MatchHistoryCubit>().getMatchHistory();
-          context.read<ProfileCubit>().fetchProfile();
-          context.read<ChampionCubit>().fetchTopThree();
-
-          // Show success dialog
-          showDialog(
-            context: context,
-            barrierDismissible: false, // force user to press button
-            builder: (dialogContext) => const SuccessDialog(),
-          );
-        } else if (state is AddMatchFailure) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(state.error), backgroundColor: Colors.red),
+      listener: (final context, final state) {
+        if (state.isSuccess) {
+          AppDialogs.showSuccess(
+            context,
+            title: "Match Added",
+            message: "Match added successfully",
+            onPressed: () {
+              context.read<AddMatchCubit>().resetMatchData();
+            },
           );
         }
       },
-      builder: (context, state) {
-        final isLoading = state is AddMatchLoading;
+      builder: (final context, final state) {
+        final isEnabled = context.read<AddMatchCubit>().canSubmit();
 
-        return AddMatchButton(
-          isEnabled: _isAddButtonEnabled,
-          isLoading: isLoading,
-          onPressed: _isAddButtonEnabled
-              ? () {
-                  final match = MatchModel(
-                    winnerId: winnerPlayer!,
-                    loserId: loserPlayer!,
-                    winnerScore: winnerScore,
-                    loserScore: loserScore,
-                  );
-                  // call cubit
-                  context.read<AddMatchCubit>().addMatch(match);
-                }
-              : null,
+        return OverlayLoader(
+          isLoading: state.isLoading,
+          child: SizedBox(
+            width: responsiveWidth(280),
+            child: CustomTextButton(
+              size: CustomButtonSize.large,
+              isDisabled: !isEnabled,
+              text: "Add Match",
+              onPressed: isEnabled
+                  ? () {
+                      final match = MatchModel(
+                        winnerId: state.winnerId!,
+                        loserId: state.loserId!,
+                        winnerScore: state.winnerScore,
+                        loserScore: state.loserScore,
+                      );
+                      context.read<AddMatchCubit>().addMatch(match);
+                    }
+                  : null,
+            ),
+          ),
         );
       },
     );
