7ae7ac84b6f65556302cb3a766fb068d97b1ab01
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-14 08:00:52 +0200

    Rebuild ChampionCubit And States For better ui mangment

--- lib/features/champion/logic/cubit/champion_cubit.dart
+++ lib/features/champion/logic/cubit/champion_cubit.dart
@@ -1,29 +1,26 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-import '../../data/model/champion_player_model.dart';
-import '../../data/repo/champion_repo.dart';
+import 'package:im_legends/features/champion/data/model/champion_player_model.dart';
+import 'package:im_legends/features/champion/data/repo/champion_repo.dart';
 
 part 'champion_state.dart';
 
 class ChampionCubit extends Cubit<ChampionState> {
-  final ChampionRepo repository;
+  final ChampionRepo championRepo;
 
-  ChampionCubit({required this.repository}) : super(ChampionInitial());
+  ChampionCubit({required this.championRepo}) : super(const ChampionInitial());
 
-  /// Fetch only the Top 3 champions
-  Future<void> fetchTopThree() async {
-    emit(ChampionLoading());
+  Future<void> fetchLeaderboard() async {
+    emit(const ChampionLoading());
     try {
-      final players = await repository.getTopThree();
+      final players = await championRepo.getLeaderboard();
 
       if (players.isEmpty) {
-        emit(ChampionFailure("⚠️ No champions found"));
+        emit(const ChampionEmpty());
       } else {
         emit(ChampionSuccess(players));
       }
     } catch (e) {
-      emit(ChampionFailure("❌ Failed to fetch champions: $e"));
+      emit(ChampionFailure(e.toString()));
     }
   }
 }
