028286a908b73086254429982f0d1724f8270782
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-14 06:06:30 +0200

    Rebuild History Repo with Impl

--- lib/features/history/data/repo/history_repo.dart
+++ lib/features/history/data/repo/history_repo.dart
@@ -1,23 +1,5 @@
-import '../models/match_history_card_model.dart';
-import '../service/history_service.dart';
+import 'package:im_legends/features/history/data/models/match_history_card_model.dart';
 
-class HistoryRepo {
-  final HistoryService historyService = HistoryService();
-
-  Future<List<MatchHistoryCardModel>> fetchMatches() async {
-    final matches = await historyService.fetchMatches();
-
-    return matches.map((match) {
-      return MatchHistoryCardModel(
-        matchId: match['id'] as String,
-        winnerName: match['winner']['name'] as String,
-        loserName: match['loser']['name'] as String,
-        winnerImage: match['winner']['profile_image'] as String? ?? '',
-        loserImage: match['loser']['profile_image'] as String? ?? '',
-        winnerScore: (match['winner_score'] as num).toInt(), // int
-        loserScore: (match['loser_score'] as num).toInt(), //  int
-        matchDate: DateTime.parse(match['created_at'] as String), //  DateTime
-      );
-    }).toList();
-  }
+abstract class HistoryRepo {
+  Future<List<MatchHistoryCardModel>> fetchMatches();
 }
