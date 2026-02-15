48fb0fc6cf1445597f61c9ef680122622846144c
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-15 06:36:12 +0200

    Todate date formate method & Init refresh methods

--- lib/core/utils/functions/date_formate.dart
+++ lib/core/utils/functions/date_formate.dart
@@ -1,20 +1,20 @@
 import 'package:intl/intl.dart';
 
-DateTime onlyDate(DateTime dt) => DateTime(dt.year, dt.month, dt.day);
+DateTime onlyDate(final DateTime dt) => DateTime(dt.year, dt.month, dt.day);
 
 // Short date and time
-String formatShort(DateTime dateTime) {
+String formatShort(final DateTime dateTime) {
   return DateFormat('d MMM, h:mm a').format(dateTime); // → 20 Jul, 3:45 PM
 }
 
 // Full readable format
-String formatFull(DateTime dateTime) {
+String formatFull(final DateTime dateTime) {
   return DateFormat(
     'EEE, d MMM yyyy · h:mm a',
   ).format(dateTime); // → Sat, 20 Jul 2025 · 3:45 PM
 }
 
-String formatSmart(DateTime date) {
+String dateDetailedFormat(final DateTime date) {
   final now = DateTime.now();
   final today = DateTime(now.year, now.month, now.day);
   final taskDate = DateTime(date.year, date.month, date.day);
@@ -22,8 +22,10 @@ String formatSmart(DateTime date) {
   if (taskDate == today) {
     return 'Today · ${DateFormat('h:mm a').format(date)}';
   }
+
   if (taskDate == today.add(const Duration(days: 1))) {
     return 'Tomorrow · ${DateFormat('h:mm a').format(date)}';
   }
-  return DateFormat('EEE, d MMM · h:mm a').format(date);
+
+  return DateFormat('EEE, d MMM y · h:mm a').format(date);
 }
--- lib/core/utils/functions/refresh_page.dart
+++ lib/core/utils/functions/refresh_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:im_legends/features/history/logic/cubit/match_history_cubit.dart';
 
 import '../../../features/champion/logic/cubit/champion_cubit.dart';
 import '../../../features/home/logic/cubit/home_cubit.dart';
@@ -8,8 +9,8 @@ import '../../../features/profile/logic/cubit/profile_cubit.dart';
 Future<void> refreshData(final BuildContext context) async {
   await Future.wait([
     context.read<HomeCubit>().loadLeaderboard(),
-    // context.read<MatchHistoryCubit>().getMatchHistory(),
+    context.read<MatchHistoryCubit>().fetchMatches(),
     context.read<ProfileCubit>().fetchProfile(),
-    context.read<ChampionCubit>().fetchTopThree(),
+    context.read<ChampionCubit>().fetchLeaderboard(),
   ]);
 }
