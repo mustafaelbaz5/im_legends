e7ca71cc78b234dbbfc7ba941fe2137484ca0532
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-15 06:39:34 +0200

    Set notification Icon

--- lib/features/home/ui/widgets/home_app_bar.dart
+++ lib/features/home/ui/widgets/home_app_bar.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:im_legends/core/themes/app_colors.dart';
 import 'package:im_legends/core/themes/app_texts_style.dart';
-import 'package:im_legends/core/utils/app_assets.dart';
 import 'package:im_legends/core/utils/spacing.dart';
+import 'package:im_legends/core/widgets/notification_icon.dart';
 
 import '../../../../core/utils/extensions/context_extensions.dart';
 import '../../../../core/utils/functions/app_setting_method.dart';
@@ -41,13 +39,7 @@ class HomeAppBar extends StatelessWidget {
               backgroundColor: context.customColors.divider.withValues(
                 alpha: 0.5,
               ),
-              child: SvgPicture.asset(
-                AppAssets.notificationIconSvg,
-                colorFilter: const ColorFilter.mode(
-                  AppColors.primary300,
-                  BlendMode.srcIn,
-                ),
-              ),
+              child: const NotificationIcon(unreadCount: 3),
             ),
           ),
         ],
