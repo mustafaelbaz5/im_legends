45e04a03620466dd4d4db519e0b4c162e3c2fad6
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-15 03:55:34 +0200

    Set LOcalation For Profile Screen

--- lib/features/profile/ui/widgets/profile_top_bar.dart
+++ lib/features/profile/ui/widgets/profile_top_bar.dart
@@ -1,11 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:im_legends/core/router/routes.dart';
 import 'package:im_legends/core/themes/app_colors.dart';
 import 'package:im_legends/core/themes/app_texts_style.dart';
 import 'package:im_legends/core/utils/app_assets.dart';
-import 'package:im_legends/core/utils/extensions/context_extensions.dart';
 import 'package:im_legends/core/utils/spacing.dart';
+
+import '../../../../core/utils/extensions/context_extensions.dart';
 
 class ProfileTopBar extends StatelessWidget {
   const ProfileTopBar({super.key});
@@ -26,7 +28,7 @@ class ProfileTopBar extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Profile',
+              'profile.profile'.tr(),
               style: AppTextStyles.font20Bold.copyWith(color: AppColors.grey0),
               textAlign: TextAlign.center,
             ),
