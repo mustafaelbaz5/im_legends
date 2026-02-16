f8c14c8488e94714264708c8eb49b97d5b9a279a
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-16 04:17:41 +0200

    Refcator App Router And routes name to only the used routes

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,71 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:im_legends/core/di/dependency_injection.dart';
-import 'package:im_legends/features/add_match/logic/cubit/add_match_cubit.dart';
-import 'package:im_legends/features/auth/logic/cubit/auth_cubit.dart';
 import 'package:im_legends/features/auth/ui/login_screen.dart';
 import 'package:im_legends/features/auth/ui/sign_up_screen.dart';
-import 'package:im_legends/features/champion/logic/cubit/champion_cubit.dart';
-import 'package:im_legends/features/history/logic/cubit/match_history_cubit.dart';
-import 'package:im_legends/features/home/logic/cubit/home_cubit.dart';
-import 'package:im_legends/features/main_navigation/ui/main_scaffold.dart';
 import 'package:im_legends/features/notification/ui/notifications_screen.dart';
-import 'package:im_legends/features/profile/logic/cubit/profile_cubit.dart';
 
-import '../../features/onboarding/ui/on_boarding_screen.dart';
 import 'routes.dart';
 
 class AppRouter {
   Route<dynamic>? generateRoute(final RouteSettings settings) {
-    // final args = settings.arguments;
+    // ignore: unused_local_variable
+    final args = settings.arguments;
 
     switch (settings.name) {
-      // ----------------- ONBOARDING -----------------
-      case Routes.onBoardingScreen:
-        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
-
-      // ----------------- AUTH -----------------
       case Routes.loginScreen:
-        return MaterialPageRoute(
-          builder: (_) => _withAuthProviders(const LoginScreen()),
-        );
-
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
       case Routes.signUpScreen:
-        return MaterialPageRoute(
-          builder: (_) => _withAuthProviders(const SignUpScreen()),
-        );
+        return MaterialPageRoute(builder: (_) => const SignUpScreen());
 
-      // ----------------- MAIN -----------------
-      case Routes.mainScaffold:
-        return MaterialPageRoute(
-          builder: (_) => _withMainProviders(const MainScaffold()),
-        );
+      // Notifications
       case Routes.notificationsScreen:
-        return MaterialPageRoute(
-          builder: (_) => _withMainProviders(const NotificationsScreen()),
-        );
+        return MaterialPageRoute(builder: (_) => const NotificationsScreen());
 
       default:
         return null;
     }
   }
-}
-
-Widget _withAuthProviders(final Widget child) {
-  return BlocProvider(create: (_) => getIt<AuthCubit>(), child: child);
-}
-
-Widget _withMainProviders(final Widget child) {
-  return MultiBlocProvider(
-    providers: [
-      BlocProvider(create: (_) => getIt<HomeCubit>()..loadLeaderboard()),
-      BlocProvider(
-        create: (_) => getIt<MatchHistoryCubit>()..fetchMatches(),
-      ),
-      BlocProvider(create: (_) => getIt<AddMatchCubit>()..getPlayersList()),
-      BlocProvider(create: (_) => getIt<ProfileCubit>()..fetchProfile()),
-      BlocProvider(create: (_) => getIt<ChampionCubit>()..fetchLeaderboard()),
-    ],
-    child: child,
-  );
 }
--- lib/core/router/routes.dart
+++ lib/core/router/routes.dart
@@ -1,20 +1,16 @@
 abstract class Routes {
   // Public routes
-  static const String onBoardingScreen = '/onboarding';
-  static const String loginScreen = '/login';
-  static const String signUpScreen = '/signup';
+  static const String loginScreen = '/loginScreen';
+  static const String signUpScreen = '/signUpScreen';
 
   // Protected main navigation routes
-  static const String mainScaffold = '/main-scaffold';
-  static const String homeScreen = '/home';
-  static const String addMatchScreen = '/add-match';
-  static const String championsScreen = '/champions';
-  static const String historyScreen = '/history';
-  static const String profileScreen = '/profile';
+  static const String homeScreen = '/homeScreen';
+  static const String addMatchScreen = '/addMatchScreen';
+  static const String championsScreen = '/championsScreen';
+  static const String historyScreen = '/historyScreen';
+  static const String profileScreen = '/profileScreen';
 
   // Protected standalone routes
-  static const String notificationsScreen = '/notifications';
-  static const String notificationDetailsScreen = '/notification-details';
-
-
+  static const String notificationsScreen = '/notificationsScreen';
+  static const String notificationDetailsScreen = '/notificationDetailsScreen';
 }
