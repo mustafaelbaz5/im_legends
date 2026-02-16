e90b40253175871ce38e185843c327e08c078a17
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-16 04:18:59 +0200

    Refactor Login & SinUp Screens To set BlocConsumer for auth cubit for better ui states

--- lib/features/auth/ui/login_screen.dart
+++ lib/features/auth/ui/login_screen.dart
@@ -1,11 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:im_legends/core/router/routes.dart';
+import 'package:im_legends/core/ui/dialogs/app_dialogs.dart';
+import 'package:im_legends/core/ui/loaders/overlay_loader.dart';
 import 'package:im_legends/core/utils/extensions/context_extensions.dart';
+import 'package:im_legends/features/auth/logic/cubit/auth_cubit.dart';
+import 'package:im_legends/features/auth/ui/widgets/login_form.dart';
 
 import '../../../core/themes/app_texts_style.dart';
 import '../../../core/utils/spacing.dart';
-import 'widgets/login_bloc_consumer.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -14,55 +18,67 @@ class LoginScreen extends StatelessWidget {
   Widget build(final BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: responsiveWidth(16),
-            vertical: responsiveHeight(16),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                verticalSpacing(40),
-                Text(
-                  'auth.login_title'.tr(),
-                  style: AppTextStyles.font20Bold,
-                  textAlign: TextAlign.center,
+        child: BlocConsumer<AuthCubit, AuthState>(
+          listener: (final context, final state) {
+            if (state is AuthError) {
+              AppDialogs.showError(context, message: state.error.messageKey);
+            }
+          },
+          builder: (final context, final state) {
+            return OverlayLoader(
+              isLoading: state is AuthLoading,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: responsiveWidth(16),
+                  vertical: responsiveHeight(8),
                 ),
-                verticalSpacing(12),
-                Text(
-                  'auth.login_desc'.tr(),
-                  style: AppTextStyles.font14Regular.copyWith(
-                    color: context.customColors.textSecondary,
-                    height: 1.8,
-                  ),
-                ),
-                verticalSpacing(80),
-
-                const LoginBlocConsumer(),
-                verticalSpacing(60),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'auth.dont_have_account'.tr(),
-                      style: AppTextStyles.font12Regular,
-                    ),
-                    GestureDetector(
-                      onTap: () =>
-                          context.pushReplacementNamed(Routes.signUpScreen),
-                      child: Text(
-                        'auth.sign_up'.tr(),
-                        style: AppTextStyles.font12SemiBold.copyWith(
-                          color: context.customColors.accentBlue,
-                          decorationThickness: 2,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      verticalSpacing(40),
+                      Text(
+                        'auth.login_title'.tr(),
+                        style: AppTextStyles.font20Bold,
+                        textAlign: TextAlign.center,
+                      ),
+                      verticalSpacing(12),
+                      Text(
+                        'auth.login_desc'.tr(),
+                        style: AppTextStyles.font14Regular.copyWith(
+                          color: context.customColors.textSecondary,
+                          height: 1.8,
                         ),
                       ),
-                    ),
-                  ],
+                      verticalSpacing(80),
+                      const LoginForm(),
+                      verticalSpacing(60),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'auth.dont_have_account'.tr(),
+                            style: AppTextStyles.font12Regular,
+                          ),
+                          GestureDetector(
+                            onTap: () => context.pushReplacementNamed(
+                              Routes.signUpScreen,
+                            ),
+                            child: Text(
+                              'auth.sign_up'.tr(),
+                              style: AppTextStyles.font12SemiBold.copyWith(
+                                color: context.customColors.accentBlue,
+                                decorationThickness: 2,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
--- lib/features/auth/ui/sign_up_screen.dart
+++ lib/features/auth/ui/sign_up_screen.dart
@@ -1,12 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:im_legends/core/ui/dialogs/app_dialogs.dart';
+import 'package:im_legends/core/ui/loaders/overlay_loader.dart';
 import 'package:im_legends/core/utils/extensions/context_extensions.dart';
+import 'package:im_legends/features/auth/logic/cubit/auth_cubit.dart';
+import 'package:im_legends/features/auth/ui/widgets/sign_up_form.dart';
 
 import '../../../core/router/routes.dart';
 import '../../../core/themes/app_texts_style.dart';
 import '../../../core/utils/spacing.dart';
-import 'widgets/sign_up_bloc_consumer.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
@@ -15,42 +18,59 @@ class SignUpScreen extends StatelessWidget {
   Widget build(final BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 8.h),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                verticalSpacing(40),
-                Text(
-                  'auth.sign_up_title'.tr(),
-                  style: AppTextStyles.font20Bold,
+        child: BlocConsumer<AuthCubit, AuthState>(
+          listener: (final context, final state) {
+            if (state is AuthError) {
+              AppDialogs.showError(context, message: state.error.messageKey);
+            }
+          },
+          builder: (final context, final state) {
+            return OverlayLoader(
+              isLoading: state is AuthLoading,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: responsiveWidth(16),
+                  vertical: responsiveHeight(8),
                 ),
-                verticalSpacing(32),
-                const SignUpBlocConsumer(),
-                verticalSpacing(8),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'auth.already_have_account'.tr(),
-                      style: AppTextStyles.font12Regular,
-                    ),
-                    GestureDetector(
-                      onTap: () => context.pushReplacementNamed(Routes.loginScreen),
-                      child: Text(
-                        'auth.login'.tr(),
-                        style: AppTextStyles.font12SemiBold.copyWith(
-                          color: context.customColors.accentBlue,
-                          decorationThickness: 2,
-                        ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      verticalSpacing(40),
+                      Text(
+                        'auth.sign_up_title'.tr(),
+                        style: AppTextStyles.font20Bold,
                       ),
-                    ),
-                  ],
+                      verticalSpacing(32),
+                      const SignUpForm(),
+                      verticalSpacing(8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'auth.already_have_account'.tr(),
+                            style: AppTextStyles.font12Regular,
+                          ),
+                          GestureDetector(
+                            onTap: () => context.pushReplacementNamed(
+                              Routes.loginScreen,
+                            ),
+                            child: Text(
+                              'auth.login'.tr(),
+                              style: AppTextStyles.font12SemiBold.copyWith(
+                                color: context.customColors.accentBlue,
+                                decorationThickness: 2,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      verticalSpacing(16),
+                    ],
+                  ),
                 ),
-                verticalSpacing(16),
-              ],
-            ),
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
