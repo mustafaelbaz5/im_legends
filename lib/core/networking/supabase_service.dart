1a8476e17ae4a7be0511c03df06b316b7e9030b9
mustafaelbaz5 <m9stafa05@gmail.com>
2026-03-15 14:49:42 +0200

    Update Subpabase Service to Set time Out

--- lib/core/networking/supabase_service.dart
+++ lib/core/networking/supabase_service.dart
@@ -1,9 +1,18 @@
+import 'package:im_legends/core/errors/exceptions.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SupabaseService {
+  static const _timeout = Duration(seconds: 15);
+
   SupabaseClient get client => Supabase.instance.client;
-
-  Future<void> signOut() => client.auth.signOut();
-
   User? get currentUser => client.auth.currentUser;
+
+  Future<void> signOut() => execute(client.auth.signOut());
+
+  Future<T> execute<T>(final Future<T> future) {
+    return future.timeout(
+      _timeout,
+      onTimeout: () => throw RequestTimeoutException(),
+    );
+  }
 }
