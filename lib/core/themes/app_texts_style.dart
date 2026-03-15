460a3cd97a7daa20c95cae342a14c0cb73858523
mustafaelbaz5 <m9stafa05@gmail.com>
2026-03-15 13:49:24 +0200

    Update App TExtStyles

--- lib/core/themes/app_texts_style.dart
+++ lib/core/themes/app_texts_style.dart
@@ -1,81 +1,92 @@
 import 'package:flutter/material.dart';
+
 import 'app_font_weight.dart';
-import '../utils/spacing.dart';
 
 class AppTextStyles {
   const AppTextStyles._();
 
   static const TextStyle fontBold = TextStyle(fontWeight: AppFontWeight.bold);
+
   static const TextStyle fontSemiBold = TextStyle(
     fontWeight: AppFontWeight.semiBold,
   );
+
   static const TextStyle fontRegular = TextStyle(
     fontWeight: AppFontWeight.regular,
   );
 
   // Font Size 20
-  static TextStyle font20Bold = TextStyle(
-    fontSize: rf(20),
+  static const TextStyle font20Bold = TextStyle(
+    fontSize: 20,
     fontWeight: AppFontWeight.bold,
   );
-  static TextStyle font20SemiBold = TextStyle(
-    fontSize: rf(20),
+
+  static const TextStyle font20SemiBold = TextStyle(
+    fontSize: 20,
     fontWeight: AppFontWeight.semiBold,
   );
-  static TextStyle font20Regular = TextStyle(
-    fontSize: rf(20),
+
+  static const TextStyle font20Regular = TextStyle(
+    fontSize: 20,
     fontWeight: AppFontWeight.regular,
   );
 
   // Font Size 18
-  static TextStyle font18Bold = TextStyle(
-    fontSize: rf(18),
+  static const TextStyle font18Bold = TextStyle(
+    fontSize: 18,
     fontWeight: AppFontWeight.bold,
   );
-  static TextStyle font18SemiBold = TextStyle(
-    fontSize: rf(18),
+
+  static const TextStyle font18SemiBold = TextStyle(
+    fontSize: 18,
     fontWeight: AppFontWeight.semiBold,
   );
-  static TextStyle font18Regular = TextStyle(
-    fontSize: rf(18),
+
+  static const TextStyle font18Regular = TextStyle(
+    fontSize: 18,
     fontWeight: AppFontWeight.regular,
   );
 
   // Font Size 16
-  static TextStyle font16Bold = TextStyle(
-    fontSize: rf(16),
+  static const TextStyle font16Bold = TextStyle(
+    fontSize: 16,
     fontWeight: AppFontWeight.bold,
   );
-  static TextStyle font16SemiBold = TextStyle(
-    fontSize: rf(16),
+
+  static const TextStyle font16SemiBold = TextStyle(
+    fontSize: 16,
     fontWeight: AppFontWeight.semiBold,
   );
-  static TextStyle font16Regular = TextStyle(
-    fontSize: rf(16),
+
+  static const TextStyle font16Regular = TextStyle(
+    fontSize: 16,
     fontWeight: AppFontWeight.regular,
   );
 
   // Font Size 14
-  static TextStyle font14Bold = TextStyle(
-    fontSize: rf(14),
+  static const TextStyle font14Bold = TextStyle(
+    fontSize: 14,
     fontWeight: AppFontWeight.bold,
   );
-  static TextStyle font14SemiBold = TextStyle(
-    fontSize: rf(14),
+
+  static const TextStyle font14SemiBold = TextStyle(
+    fontSize: 14,
     fontWeight: AppFontWeight.semiBold,
   );
-  static TextStyle font14Regular = TextStyle(
-    fontSize: rf(14),
+
+  static const TextStyle font14Regular = TextStyle(
+    fontSize: 14,
     fontWeight: AppFontWeight.regular,
   );
 
   // Font Size 12
-  static TextStyle font12Regular = TextStyle(
-    fontSize: rf(12),
+  static const TextStyle font12Regular = TextStyle(
+    fontSize: 12,
     fontWeight: AppFontWeight.regular,
   );
-  static TextStyle font12SemiBold = TextStyle(
-    fontSize: rf(12),
+
+  static const TextStyle font12SemiBold = TextStyle(
+    fontSize: 12,
     fontWeight: AppFontWeight.semiBold,
   );
 }
